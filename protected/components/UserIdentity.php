14cd6e701e99c1adfe4d38e419843bbc77a23a78
tylerdurdn <tylerdurdn2010@gmail.com>
2014-11-02 09:15:24 -0500

    user login

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,7 +7,13 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	/**
+	private $_id;
+
+	public function getId()
+	{
+		return $this->_id;
+	}
+        /**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
 	 * are both 'demo'.
@@ -17,17 +23,29 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+
+		$user = User::model()->findByAttributes(array('username'=>$this->username));
+		if ($user ==null)
+		{
+			$this->errorCode = self::ERROR_USERNAME_INVALID;
+		}
+
 		else
-			$this->errorCode=self::ERROR_NONE;
+		{	
+			if($user->password != $user->encrypt($this->password))
+			{
+				$this->errorCode = self::ERROR_PASSWORD_INVALID;
+			}
+			else
+			{
+				$this->_id = $user->userid;
+				$this->errorCode = self::ERROR_NONE;
+
+			}
+			
+		}
 		return !$this->errorCode;
 	}
+
+
 }
